9bccb4224aa3b43279840c38b79f61a8087a20e6
Hector Esquerdo Valverde <hector@hevprojects.com>
2024-08-01 10:52:58 +0200

    Dialog

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -109,3 +109,19 @@ test('tooltips', async ({ page }) => {
   expect(tooltip).toEqual('This is a tooltip')
 
 })
+
+test('dialogs', async ({ page }) => {
+  await page.getByText("Tables & Data").click();
+  await page.getByText("Smart Table").click();
+
+  // when there are a dialog that not is from the web, is from the browser, and you cannot detect the html item
+  // This is type listener
+  page.on('dialog', dialog => {
+    expect(dialog.message()).toContain('Are you sure you want to delete?')
+    dialog.accept()
+  })
+
+  await page.getByRole('table').locator('tr', {hasText: 'mdo@gmail.com'}).locator('.nb-trash').click()
+  await expect(page.locator('table tr').first()).not.toHaveText('mdo@gmail.com')
+
+})
